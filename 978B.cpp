35e536a6cbdeadc36e48da2dce62ef8e91ad8a77
yashwardhan-gautam <gautamyashwardhan@gmail.com>
2020-09-04 23:31:16 +0530

    978B

---
+++ 978B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi              first
+#define se              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+void fast()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main()
+{
+	fast();
+	int n;	cin >> n;
+	string s;	cin >> s;
+	int count = 0;
+	while (s.find("xxx") != string::npos)
+	{
+		s.erase(s.begin() + s.find("xxx"));
+		count++;
+		//cout << s << endl;
+	}
+	cout << count << endl;
+	return 0;
+}
