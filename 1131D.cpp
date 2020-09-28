4d2a8ff7117d4f4ad1c795d4baab69eba69483e3
yashwardhan-gautam <gautamyashwardhan@gmail.com>
2020-09-28 12:22:55 +0530

    Gourmet choice code changed

--- 1131D.cpp
+++ 1131D.cpp
@@ -1,4 +1,3 @@
-// https://codeforces.com/problemset/problem/1131/D
 #include<bits/stdc++.h>
 using namespace std;
 #define fi              first
@@ -15,7 +14,7 @@ using namespace std;
 #define zrobits(x)      __builtin_ctzll(x)
 #define mod             1000000007
 #define inf             1e18
-#define endl		 	      "\n"
+#define endl            "\n"
 void fast()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -24,63 +23,63 @@ void fast()
 	freopen("output.txt", "w", stdout);
 #endif
 }
-vector<pair<int, int> > edges;
-vector<int> adj[3004];
-bool cycle = false;
-// DSU with elements 0,1,2....n-1
-class dsu
+int n, m;
+vector<int> parent;
+vector<int> ra;
+vector<pii> edges;
+vector<vector<int> > adj;
+vector<bool> vis;
+vector<int> depth;
+vector<int> instack;
+int get_parent(int a)
 {
-public:
-	vector<int> parent, rank;
-	int total_components;
-	dsu(int n)
+	if (a == parent[a])
+		return a;
+	return parent[a] = get_parent(parent[a]);
+}
+void join(int a, int b)
+{
+	a = get_parent(a);
+	b = get_parent(b);
+	if (a != b)
 	{
-		parent.resize(n);
-		rank.resize(n);
-		for (int i = 0; i < n; i++)
-		{
-			parent[i] = i;
-			rank[i] = 0;
-		}
-		total_components = n;
+		if (ra[b] > ra[a])
+			swap(a, b);
+		parent[b] = a;
+		if (ra[a] == ra[b])
+			ra[a]++;
 	}
-	int get(int a)
+}
+bool cycle_rec(int node)
+{
+	vis[node] = true;
+	instack[node] = true;
+	for (auto child : adj[node])
 	{
-		if (a == parent[a])
-			return a;
-		return parent[a] = get(parent[a]);
+		if (!vis[child] and cycle_rec(child))
+			return true;
+		if (instack[child])
+			return true;
 	}
-	void join(int a, int b)
-	{
-		a = get(a);
-		b = get(b);
-		if (a != b)
-		{
-			if (rank[b] > rank[a])
-				swap(a, b);
-			parent[b] = a;
-			if (rank[a] == rank[b])
-				rank[a]++;
-			total_components--;
-		}
-	}
-};
-void find_depth(int node, unordered_map<int, bool> &vis, vector<int> &depth)
+	instack[node] = false;
+	return false;
+}
+bool cycle_exist()
+{
+	for (int i = 0; i < n + m; i++)
+		if (!vis[get_parent(i)] and cycle_rec(i))
+			return true;
+	return false;
+}
+void find_depth(int node)
 {
 	if (depth[node] != -1)
 		return ;
-	if (cycle)
-		return ;
-	if (vis[node])
-	{
-		cycle = true;
-		return ;
-	}
 	vis[node] = true;
 	int d = 0;
 	for (auto child : adj[node])
 	{
-		find_depth(child, vis, depth);
+		find_depth(child);
 		d = max(d, depth[child]);
 	}
 	depth[node] = d + 1;
@@ -89,41 +88,47 @@ void find_depth(int node, unordered_map<int, bool> &vis, vector<int> &depth)
 int32_t main()
 {
 	fast();
-	int n, m;	cin >> n >> m;
-	dsu d(n + m);
+	cin >> n >> m;
+	parent.resize(n + m);
+	ra.resize(n + m);
+	for (int i = 0; i < parent.size(); i++)
+	{
+		parent[i] = i;
+		ra[i] = 0;
+	}
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			char ch;	cin >> ch;
+			char ch;    cin >> ch;
 			if (ch == '=')
-				d.join(i, j + n);
+				join(i, n + j);
 			else if (ch == '>')
-				edges.pb(mp(i, n + j));
+				edges.pb({i, n + j});
 			else
-				edges.pb(mp(n + j, i));
+				edges.pb({n + j, i});
 		}
 	}
+	adj.resize(n + m), vis.resize(n + m, false), instack.resize(n + m, false), depth.resize(n + m, -1);
+	// make the graph just considering the super parents
 	for (auto e : edges)
-		adj[d.get(e.fi)].pb(d.get(e.se));
-	vector<int> depth(n + m, -1);
-	unordered_map<int, bool> vis;
+		adj[get_parent(e.fi)].pb(get_parent(e.se));
+	if (cycle_exist())
+	{
+		cout << "No\n";
+		return 0;
+	}
 	for (int i = 0; i < n + m; i++)
-	{
-		if (!vis[d.get(i)]) {
-			find_depth(i, vis, depth);
-		}
-	}
-	if (cycle)
-		cout << "NO\n";
-	else
-	{
-		cout << "YES\n";
-		for (int i = 0; i < n; i++)
-			cout << depth[d.get(i)] << " ";	cout << endl;
-		for (int j = 0; j < m; j++)
-			cout << depth[d.get(n + j)] << " ";
-		cout << endl;
-	}
+		vis[i] = false;
+	for (int i = 0; i < n + m; i++)
+		if (!vis[i])
+			find_depth(i);
+	cout << "Yes\n";
+	for (int i = 0; i < n; i++)
+		cout << depth[get_parent(i)] << " ";
+	cout << endl;
+	for (int j = 0; j < m; j++)
+		cout << depth[get_parent(n + j)] << " ";
+	cout << endl;
 	return 0;
 }
